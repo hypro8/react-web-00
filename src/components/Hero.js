4a907c397afb0298241550593aee70aa52afb9f2
Yang <hao.frank@gmail.com>
2020-10-08 23:47:08 +0100

    init commit

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div>
+            <ww><ul>
+                <li></li>
+            </ul></ww>
+        </div>
+    )
+}
+
+export default Hero
