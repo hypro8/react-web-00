dd7b3339eb7a081d6b7fff4ed13465810b52db30
Yang <hao.frank@gmail.com>
2020-10-09 00:46:08 +0100

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path='/' exact />
-      </Switch>
-
-    </Router>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Router>
     </>
   );
 }
